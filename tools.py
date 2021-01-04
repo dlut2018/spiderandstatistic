39d8fcf7a5fc6a8840a21b8c81000733c96cda88
Blackwhite2 <57354630+Blackwhite2@users.noreply.github.com>
2021-01-04 18:11:24 +0800

    Update tools.py
    增加数据处理函数

--- tools.py
+++ tools.py
@@ -13,5 +13,52 @@ def getprices(num):
     #数据处理
     for i in range(0, len(prices)):
         prices[i] = float(str(prices[i])[2:-3])
-
     return prices
+
+#处理从数据库中读取的评论数字符串 返回整数
+def handleComm(comm_str):
+    target = str(comm_str)
+
+    if len(target) == 0:
+        return 0
+
+    #去掉前后的无关符号
+    target = target[2:-3]
+
+    #去加号
+    if target.find('+') != -1:
+        target = target[:-1]
+
+    #处理'万'
+    if target.find('万') != -1:
+        target = target[:-1]
+        return int(float(target)*10000)
+
+    if target =='':
+        return 0
+
+    return int(float(target))
+
+#获取评论数列表 --- 待优化
+def getComms(num):
+    conn = sqlite3.connect("test.db")
+    c = conn.cursor()
+    sql = "select goodevnum from goodsinfo limit 1," + str(num)
+    c.execute(sql)
+    comms = c.fetchall()
+    conn.close()
+    # 数据处理
+    for i in range(0, len(comms)):
+        comms[i] = handleComm(comms[i])
+    return comms
+
+#获得价格与评论数字典
+def price_comm_data(num,scope):
+    data = {}
+    prices = getprices(num)
+    comms = getComms(num)
+
+    for i in range(0,len(prices)):
+        if prices[i]>=scope[0] and prices[i]<=scope[1]:
+            data[prices[i]] = data.get(prices[i],0) + comms[i]
+    return data
